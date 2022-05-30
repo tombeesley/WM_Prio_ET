f6eb26e882aeca412208e1fc0399c24c10ad7a85
tombeesley <t.beesley@lancaster.ac.uk>
2022-05-30 16:57:55 +0100

    analysis of pilot data

--- Analysis/process_eye_data.R
+++ Analysis/process_eye_data.R
@@ -1,25 +1,63 @@
 library(tidyverse)
 library(eyetools) # github.com/tombeesley/eyetools
+library(patchwork)
 
-d <- readRDS("data_pilot.RDS")
+theme_set(theme_light())
+
+d_raw <- readRDS("data_pilot.RDS")
+
+d_raw <- combine_eyes(d_raw,"average")
 
 # change to screen coordinates
 d <- 
-  d %>% 
-  mutate(x = x*1920, y = y*1080) 
+  d_raw %>% 
+  mutate(x = x*1920, y = y*1080)
 
-d7 <- 
+d_s <- 
   d %>% 
   filter(trial_phase == 7) %>% 
-  select(-trial_phase) %>% # eyetools does not recognise this column.
-  filter(trial == 18)
+  select(-trial_phase) %>% 
+  filter(between(trial, 1, 140))
 
 # process fixations
-fix7 <- 
-  eyetools::fix_dispersion(d7)
+f_disp <- 
+  eyetools::fix_dispersion(d_s, disp_tol = 75)
+
+
+# how many fixations per trial
+fix_summary <- 
+  f_disp %>% 
+  group_by(trial) %>% 
+  summarise(nFix = n(),
+            meanDur = mean(dur))
+
+# distribution of fixation counts
+nFixplot <- 
+  fix_summary %>% 
+  ggplot() +
+  geom_histogram(aes(nFix))
+
+# distribution of fixation durations
+meanDurPlot <- 
+  fix_summary %>% 
+  ggplot() +
+  geom_histogram(aes(meanDur))
+
+nFixplot + meanDurPlot
+
+
+# f_vel <- 
+#   eyetools::VTI_saccade(d_s)
+
+
+AOIs <- data.frame(x = c(840,840, 1080, 1080),
+                   y = c(660,420, 660, 420), 
+                   width = c(50,50,50,50),
+                   height = c(50,50,50,50))
 
 #plot the fixations and raw data
-spatial_plot(raw_data = d7, 
-             fix_data = fix7, 
-             res = c(0,1920,0,1080), 
-             show_fix_order = FALSE)
+spatial_plot(raw_data = d_s, 
+             fix_data = f_disp,
+             AOIs = AOIs,
+             res = c(400,1520,200,880), 
+             show_fix_order = TRUE)
