70aca510a58fedbf79b2047324d28d5c21d6c816
tombeesley <t.beesley@lancaster.ac.uk>
2022-06-16 14:51:09 +0100

    added pilot analysis

--- Analysis/process_eye_data.R
+++ Analysis/process_eye_data.R
@@ -4,9 +4,10 @@ library(patchwork)
 
 theme_set(theme_light())
 
-d_raw <- readRDS("data_pilot.RDS")
+#d_raw <- readRDS("data_pilot.RDS")
+load("data_pilot.RData")
 
-d_raw <- combine_eyes(d_raw,"average")
+d_raw <- combine_eyes(d_spaced_3s,"average")
 
 # change to screen coordinates
 d <- 
@@ -17,7 +18,7 @@ d_s <-
   d %>% 
   filter(trial_phase == 7) %>% 
   select(-trial_phase) %>% 
-  filter(between(trial, 1, 140))
+  filter(between(trial, 31, 31))
 
 # process fixations
 f_disp <- 
@@ -29,7 +30,7 @@ fix_summary <-
   f_disp %>% 
   group_by(trial) %>% 
   summarise(nFix = n(),
-            meanDur = mean(dur))
+            meanDur = mean(duration))
 
 # distribution of fixation counts
 nFixplot <- 
@@ -50,14 +51,14 @@ nFixplot + meanDurPlot
 #   eyetools::VTI_saccade(d_s)
 
 
-AOIs <- data.frame(x = c(840,840, 1080, 1080),
-                   y = c(660,420, 660, 420), 
-                   width = c(50,50,50,50),
-                   height = c(50,50,50,50))
+AOIs <- data.frame(x = c(460,460, 1460, 1460),
+                   y = c(1040,40, 1040, 40), 
+                   width = c(60,60,60,60),
+                   height = c(60,60,60,60))
 
 #plot the fixations and raw data
 spatial_plot(raw_data = d_s, 
              fix_data = f_disp,
              AOIs = AOIs,
-             res = c(400,1520,200,880), 
+             res = c(0,1920,0,1080), 
              show_fix_order = TRUE)
